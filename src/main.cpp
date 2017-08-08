890ca1418cbd6039fb519e00b51249320b65268b
Wolfgang90 <eichler_wolfgang@msn.com>
2017-08-08 20:23:53 +0200

    Minor clean-ups in main()

--- src/main.cpp
+++ src/main.cpp
@@ -208,9 +208,6 @@ int main() {
 
             //ego_car.update_current_status( 1.0, 1.0, 1.0, 1.0, 1.0, 1.0); 
 
-          	// Previous path data given to the Planner
-          	auto previous_path_x = j[1]["previous_path_x"];
-          	auto previous_path_y = j[1]["previous_path_y"];
           	// Previous path's end s and d values 
           	double end_path_s = j[1]["end_path_s"];
           	double end_path_d = j[1]["end_path_d"];
