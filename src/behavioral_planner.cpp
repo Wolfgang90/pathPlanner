57e3f7608c809a43195627db254f772706d55b27
Wolfgang90 <eichler_wolfgang@msn.com>
2017-08-17 12:04:00 +0200

    Cleaned up behavioral_planner.cpp

--- src/behavioral_planner.cpp
+++ src/behavioral_planner.cpp
@@ -7,7 +7,7 @@ Behavioral_planner::Behavioral_planner(Ego ego_car_, vector<vector<double>> sens
   // Number of lanes
   num_lanes = 3;
   // Buffer value in front and behind of a car position not to be occupied by the own car
-  car_buffer = 5.0;
+  car_buffer = 20.0;
   // Time of prediction in the future
   dt = {1.0,2.0,3.0};
   // Weights for each prediction in the future
@@ -39,8 +39,6 @@ Behavioral_planner::Planned Behavioral_planner::plan(){
   /*
    * BEGIN: Simple return values
    */
-  //Planned planned;
-  //planned.lane = 1;
   planned.speed = maximum_speed;
   /*
    * END: Simple return values
@@ -59,7 +57,6 @@ void Behavioral_planner::update_cars(){
 
 void Behavioral_planner::determine_lane_costs(){
 
-
   for(int i = 0; i < dt.size(); i++){
     
     double ego_car_s = ego_car.s_predictions[dt[i]];
@@ -73,7 +70,6 @@ void Behavioral_planner::determine_lane_costs(){
     map<int,double> speedgap_in_front{{0,maximum_speed},{1,maximum_speed},{2,maximum_speed}};
 
     for (int j = 0; j < other_cars.size(); j++){
-      //
       // Extract required parameters for simplification
       double other_car_s = other_cars[j].s_predictions[dt[i]];
       int lane = other_cars[j].lane;
@@ -82,10 +78,9 @@ void Behavioral_planner::determine_lane_costs(){
       // If the other car is already in the next lap
       if(other_car_s < (ego_car_s - 1000)){
         other_car_s = other_car_s +  max_s;
-        //cout << "already in next lap" << endl;
+
       //If the other car is still in the previous lap
       } else if(other_car_s > (ego_car_s + 1000)){
-        //cout << "still in previous lap" << endl;
         other_car_s = other_car_s - max_s;
       }
       // END: Handle lap change
@@ -101,20 +96,21 @@ void Behavioral_planner::determine_lane_costs(){
       }
 
       //Add costs for car in safety space
-      if(other_car_s > safety_begin && other_car_s < safety_end){
+      if(other_car_s > safety_begin && other_car_s < safety_end && ego_car.lane != lane){
         lane_cost[lane] += 1000;
       }
     }
 
-    for(int k = 0; k < num_lanes; k++){
-      lane_cost[k] = lane_cost[k] + dt_weights[i] * speedgap_in_front[k] / car_in_front[k];
-    }
-    /*
     // Add costs per lane for car in front
-    for (auto const& lane_label: lane_cost){
-      lane_cost[lane_label.first] +=  100 * dt_weights[i] * speedgap_in_front[lane_label.first] / car_in_front[lane_label.first];
+    for(int k = 0; k < num_lanes; k++){
+      lane_cost[k] = lane_cost[k] + dt_weights[i] * speedgap_in_front[k] * 10 / car_in_front[k];
     }
-    */
+  }
+
+  // Add cost for distance to current lane
+  int current_lane = ego_car.lane; 
+  for(int k = 0; k < num_lanes; k++){
+    lane_cost[k] = lane_cost[k] + abs(k-current_lane) * 0.2;
   }
 }
 
